7b7d83a894957e6eae5d8f9e5e3a4259889c78e7
CarlosSaiz90 <k4rlos_90@hotmail.com>
2026-02-02 02:52:48 +0100

    Update Main.java

--- Primer Proyecto/src/Main.java
+++ Primer Proyecto/src/Main.java
@@ -1,16 +1,60 @@
+import java.sql.SQLOutput;
+
 public class Main {
+
+    String nombremMio = "Carlos Saiz Pereira";
 
     public static void main (String[]args){
         System.out.println("hola este es mi primer progama en java");
-        System.out.println();{
+
+        String nombreCompleto = "Carlos Saiz Pereira"; //los String son palabras y van entre " ""
+        nombreCompleto = "Carlos Saiz Pereira";
+        System.out.println("mi nombre es "+nombreCompleto);
+        final String NUMERO_DNI = "72073852V";
+        int edad = 35;
+        edad = edad +1;
+        edad = edad +1;
+
+        System.out.println("mi edad es " + edad);
+
+        float altura = 1.95f;
+        double peso = 95;
+        char letraCarnet = 'V';
+        boolean experiencia = true;
+        Integer edadcompleja = 23;
+
+
+        System.out.println("mi numero de dni es "+NUMERO_DNI);
+        System.out.println("mi altura es de "+altura+"cm");
+        System.out.println("mi peso es de "+peso+"kg");
+        System.out.println("mi nombre es "+nombreCompleto);
+        System.out.println("mi nombre es "+nombreCompleto);
+        System.out.println("la letra de mi dni es "+letraCarnet);
+        System.out.println("mi experiencia es "+experiencia);
+
+
+
 
 
 
 
     }
 
-aaa
+
+
+
+
+ public void otrometodo
+
+
+
+
 
     }
+
+
+
+
+
 
 
