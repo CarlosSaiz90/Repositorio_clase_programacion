70d1aabd7a4dd605f815a2ea78a70cc8ce2bcf29
CarlosSaiz90 <k4rlos_90@hotmail.com>
2026-02-02 03:36:57 +0100

    ok

---
+++ Ejercicios/src/Main.java
@@ -0,0 +1,77 @@
+import java.sql.SQLOutput;
+import java.util.SortedMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    //EJERCICIO 1
+
+        String nombreCompleto = "Ana";
+        int edad = 22;
+        String ciudad = "Madrid";
+
+        System.out.println("El nombre de la chica es "+nombreCompleto);
+        System.out.println("la edad de " +nombreCompleto+ " es " +edad);
+        System.out.println("Ana vive en "+ciudad);
+
+    //EJERCICIO 2
+
+        int puntuacion = 0;
+        System.out.println("La puntuacion incial "+puntuacion);
+
+        puntuacion += +5;
+        System.out.println("Despues de primera modificacion "+puntuacion);
+
+        puntuacion += +5;
+        System.out.println("Despues de segunda modificacion "+puntuacion);
+
+        puntuacion += +5;
+        System.out.println("Despues de tercera modificacion "+puntuacion);
+
+
+    //EJERCICIO 3
+
+        String nombre = "Carlos";
+        int años = 35;
+        boolean estudiante = true;
+        double  altura = 1.75f;
+        char inicial = 'C';
+
+        System.out.println("el nombre del alumno es "+nombre);
+        System.out.println("el alumno tiene " +años+ " años");
+        System.out.println("el alumno " +nombre+ " es estudiante? " +estudiante);
+        System.out.println("la altura de " +nombre+ " es " +altura);
+        System.out.println("la inicial es "+inicial);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
